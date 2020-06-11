ffc45ca700d66e07e6a6cf3704a77ef994601b31
Tomek Zawadzki <tomekzawadzki1990@gmail.com>
2020-06-11 12:19:27 +0200

    add moment, and use it

--- src/js/index.js
+++ src/js/index.js
@@ -7,3 +7,13 @@ console.log('HELLO 🚀')
 import {buttonHamburger} from './buttonHamburger.js'
 
 console.log(buttonHamburger)
+
+import moment from 'moment'
+console.log(moment().startOf('day').fromNow());
+
+const data = document.querySelector('.data__paragraf--js')
+
+data.innerHTML = moment().startOf('day').fromNow()
+data.style.fontWeight = ('700')
+data.style.fontSize = ('40px')
+data.style.color = ('tomato')
