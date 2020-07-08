c8e28e4f3a4428ff9707652827e2896a0e910081
Tomek Zawadzki <tomekzawadzki1990@gmail.com>
2020-07-08 22:14:47 +0200

    add animation

--- src/js/index.js
+++ src/js/index.js
@@ -11,21 +11,21 @@ console.log(buttonHamburger);
 import moment from 'moment';
 console.log(moment().startOf('day').fromNow());
 
-const data = document.querySelector('.data__paragrafOne--js');
+const data = document.querySelector('.data__paragraf1--js');
 
 data.innerHTML = moment().startOf('day').fromNow();
 data.style.fontWeight = ('700');
 data.style.fontSize = ('16px');
 data.style.color = ('tomato');
 
-const dataTwo = document.querySelector('.data__paragrafTwo--js');
+const dataTwo = document.querySelector('.data__paragraf2--js');
 
 dataTwo.innerHTML = moment().format('MMMM Do YYYY');
 dataTwo.style.fontWeight = ('700');
 dataTwo.style.fontSize = ('16px');
 dataTwo.style.color = ('tomato');
 
-const dataThree = document.querySelector('.data__paragrafThree--js');
+const dataThree = document.querySelector('.data__paragraf3--js');
 
 dataThree.innerHTML = moment().format('LTS');
 dataThree.style.fontWeight = ('700');
